34328128122c076ec4e735f125524c2e3bc05344
Alex Bonder <lexbonder@gmail.com>
2024-10-02 14:55:38 -0400

    add array extension

---
+++ bethorwindows/Extensions/Array-NotEmpty.swift
@@ -0,0 +1,14 @@
+//
+//  Array-NotEmpty.swift
+//  bethorwindows
+//
+//  Created by Alex Reyes-Bonder on 10/2/24.
+//
+
+import Foundation
+
+extension Array {
+    var isNotEmpty: Bool {
+        !self.isEmpty
+    }
+}
