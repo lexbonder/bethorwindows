3ab820b903bc95d3b1dd0f234354f1a13b94a0b8
Alex Bonder <lexbonder@gmail.com>
2024-10-02 14:56:36 -0400

    Update ARViewContainer

--- bethorwindows/ARExperience/ARViewContainer.swift
+++ bethorwindows/ARExperience/ARViewContainer.swift
@@ -14,39 +14,36 @@ struct ARViewContainer: UIViewRepresentable {
     
     func makeUIView(context: Context) -> ARView {
         let arView = ARView(frame: .zero)
-
-        let session = arView.session
-        let config = ARImageTrackingConfiguration()
-        
-        config.isAutoFocusEnabled = true
-        config.maximumNumberOfTrackedImages = 4
-        config.trackingImages = Set(viewModel.windows.map {
-            guard let uiImage = UIImage(named: $0.image) else {
-                fatalError("Unable to load UIImage")
-            }
-            guard let cgImage = uiImage.cgImage else {
-                fatalError("Unable to convert UI Image to CGImage")
-            }
-            let refImage = ARReferenceImage(cgImage, orientation: .up, physicalWidth: 1.07)
-            refImage.name = $0.image
-            return refImage
-        })
-        
-        session.run(config, options: [.resetTracking, .removeExistingAnchors])
-        
-        let coachingOverlay = ARCoachingOverlayView()
-        coachingOverlay.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        coachingOverlay.session = session
-        coachingOverlay.goal = .verticalPlane
-        arView.addSubview(coachingOverlay)
         
         context.coordinator.arView = arView
-        session.delegate = context.coordinator
+        arView.session.delegate = context.coordinator
         
         return arView
     }
     
-    func updateUIView(_ arView: ARView, context: Context) {}
+    func updateUIView(_ arView: ARView, context: Context) {
+        if (viewModel.windows.isNotEmpty && !viewModel.trackingImagesLoaded) {
+            let session = arView.session
+            let config = ARImageTrackingConfiguration()
+            
+            config.isAutoFocusEnabled = true
+            config.maximumNumberOfTrackedImages = 4
+            config.trackingImages = Set(viewModel.windows.map {
+                guard let uiImage = UIImage(named: $0.image) else {
+                    fatalError("Unable to load UIImage")
+                }
+                guard let cgImage = uiImage.cgImage else {
+                    fatalError("Unable to convert UI Image to CGImage")
+                }
+                let refImage = ARReferenceImage(cgImage, orientation: .up, physicalWidth: 1.07)
+                refImage.name = $0.image
+                return refImage
+            })
+            
+            session.run(config, options: [.resetTracking, .removeExistingAnchors])
+            viewModel.setTrackingImagesLoaded()
+        }
+    }
     
     func makeCoordinator() -> Coordinator {
         Coordinator(viewModel: viewModel)
